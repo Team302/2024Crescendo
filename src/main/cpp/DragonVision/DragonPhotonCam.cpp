1da965ca3a5f8264b7e49bc78288e6481b80fe47
Redragon_Gamer <141690565+LeahP1029@users.noreply.github.com>
2024-01-20 14:28:27 -0500

    Renamed Yangle to Yaw and set up Yaw collection func.
    Get Target Yaw is now implemented and returned as units::angle::degree_t. Positive = counterClockwise/left. Documentation added also.

--- src/main/cpp/DragonVision/DragonPhotonCam.cpp
+++ src/main/cpp/DragonVision/DragonPhotonCam.cpp
@@ -31,10 +31,29 @@ DragonPhotonCam::DragonPhotonCam(std::string name,
 {
 }
 
-/// no op ~
-units::angle::degree_t DragonPhotonCam::GetTargetYAngle() const
+/// @brief  get Yaw of possible target.
+/// @return units::angle::degree_t - Counter Clockwize/left for positive.
+units::angle::degree_t DragonPhotonCam::GetTargetYaw() const // originally Yaw was y-angle
 {
+    // get latest detections from co-processor
+    photon::PhotonPipelineResult result = m_camera->GetLatestResult();
+
+    // check if we have detections
+    if (result.HasTargets())
+    {
+        // get the most accurate according to configured contour ranking
+        photon::PhotonTrackedTarget target = result.GetBestTarget();
+
+        // return Yaw. This also multiplys it by -1 to invert to a global use in the 302 code
+        return units::angle::degree_t(-1.0 * target.GetYaw());
+    }
+
+    // if no tag found, return 0
+    return (units::angle::degree_t)0;
 }
+
+/// @brief get TargetSkew of possible target.
+/// @return Skew casted as units::angle::degree_t. Counter Clockwize/left for positive.
 units::angle::degree_t DragonPhotonCam::GetTargetSkew() const
 {
 
@@ -55,7 +74,7 @@ units::angle::degree_t DragonPhotonCam::GetTargetSkew() const
     return (units::angle::degree_t)0;
 }
 
-units::angle::degree_t DragonPhotonCam::GetTargetYAngleRobotFrame(units::length::inch_t *targetDistOffset_RF, units::length::inch_t *targetDistfromRobot_RF) const {}
+units::angle::degree_t DragonPhotonCam::GetTargetYawRobotFrame(units::length::inch_t *targetDistOffset_RF, units::length::inch_t *targetDistfromRobot_RF) const {}
 units::angle::degree_t DragonPhotonCam::GetTargetZAngleRobotFrame(units::length::inch_t *targetDistOffset_RF, units::length::inch_t *targetDistfromRobot_RF) const {}
 units::angle::degree_t DragonPhotonCam::GetTargetZAngle() const {}
 units::time::microsecond_t DragonPhotonCam::GetPipelineLatency() const {}
