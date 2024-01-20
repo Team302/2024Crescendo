73a8c494ebb4fe798a59c94c30ce417f141af050
Aden25 <loslatterya55@stu.lok12.org>
2024-01-20 13:41:21 -0500

    Edited the auton chooser
    Added game piece choosers for the center and wing, added 4 starting positions, deleted the charging station and cube options.

--- src/main/cpp/auton/AutonSelector.cpp
+++ src/main/cpp/auton/AutonSelector.cpp
@@ -55,9 +55,8 @@ string AutonSelector::GetSelectedAutoFile()
 	autonfile += std::string("/auton/");
 	autonfile += GetAlianceColor();
 	autonfile += GetStartPos();
-	autonfile += GetNumofPiecesinauton();
-	autonfile += GetParkOnChargeStation();
-	autonfile += GetCube();
+	autonfile += GetNumofPiecesinautonWing();
+	autonfile += GetNumofPiecesinautonCenter();
 	autonfile += std::string(".xml");
 
 	auto table = nt::NetworkTableInstance::GetDefault().GetTable("auton file");
@@ -95,11 +94,6 @@ bool AutonSelector::FileExists(const std::string &name)
 	return false;
 }
 
-string AutonSelector::GetParkOnChargeStation()
-{
-	return m_chrgstatchooser.GetSelected();
-}
-
 string AutonSelector::GetAlianceColor()
 {
 	if (FMSData::GetInstance()->GetAllianceColor() == frc::DriverStation::Alliance::kRed)
@@ -117,14 +111,14 @@ string AutonSelector::GetStartPos()
 	return m_startposchooser.GetSelected();
 }
 
-string AutonSelector::GetNumofPiecesinauton()
+string AutonSelector::GetNumofPiecesinautonWing()
 {
-	return m_numofgamepiecechooser.GetSelected();
+	return m_numofgamepiecewing.GetSelected();
 }
 
-string AutonSelector::GetCube()
+string AutonSelector::GetNumofPiecesinautonCenter()
 {
-	return m_cubechooser.GetSelected();
+	return m_numofgamepiececenter.GetSelected();
 }
 
 //---------------------------------------------------------------------
@@ -136,22 +130,23 @@ string AutonSelector::GetCube()
 void AutonSelector::PutChoicesOnDashboard()
 {
 
-	m_startposchooser.AddOption("Gridcoop", "COOP");
-	m_startposchooser.AddOption("Gridwall", "Wall");
-	m_startposchooser.AddOption("Gridhp", "HP");
+	m_startposchooser.AddOption("Amp", "Wing1");
+	m_startposchooser.AddOption("Subwoofer", "Wing2");
+	m_startposchooser.AddOption("Podium", "Wing3");
+	m_startposchooser.AddOption("Wide", "Wing4");
 	frc::SmartDashboard::PutData("StartPos", &m_startposchooser);
 
-	m_numofgamepiecechooser.AddOption("1", "One");
-	m_numofgamepiecechooser.AddOption("2", "Two");
-	m_numofgamepiecechooser.AddOption("3", "Three");
-	m_numofgamepiecechooser.AddOption("4", "Four");
-	frc::SmartDashboard::PutData("Numofpcs", &m_numofgamepiecechooser);
+	m_numofgamepiecewing.AddOption("0", "Zero");
+	m_numofgamepiecewing.AddOption("1", "One");
+	m_numofgamepiecewing.AddOption("2", "Two");
+	m_numofgamepiecewing.AddOption("3", "Three");
+	frc::SmartDashboard::PutData("NumofWingpcs", &m_numofgamepiecewing);
 
-	m_chrgstatchooser.AddOption("yes", "P");
-	m_chrgstatchooser.AddOption("no", "NP");
-	frc::SmartDashboard::PutData("park on charg station", &m_chrgstatchooser);
-
-	m_cubechooser.AddOption("Yes", "Cube");
-	m_cubechooser.AddOption("No", "");
-	frc::SmartDashboard::PutData("get cube", &m_cubechooser);
+	m_numofgamepiececenter.AddOption("0", "Zero");
+	m_numofgamepiececenter.AddOption("1", "One");
+	m_numofgamepiececenter.AddOption("2", "Two");
+	m_numofgamepiececenter.AddOption("3", "Three");
+	m_numofgamepiececenter.AddOption("4", "Four");
+	m_numofgamepiececenter.AddOption("5", "Five");
+	frc::SmartDashboard::PutData("NumofCenterpcs", &m_numofgamepiececenter);
 }
