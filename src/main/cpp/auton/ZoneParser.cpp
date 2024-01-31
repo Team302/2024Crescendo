a1d1376a6798dc42b1642f362cee2e9ddac6e5c2
Frank0624789 <89255020+Frank0624789@users.noreply.github.com>
2024-01-30 16:35:33 -0800

    Fixed the requested changes

--- src/main/cpp/auton/ZoneParser.cpp
+++ src/main/cpp/auton/ZoneParser.cpp
@@ -32,6 +32,90 @@ ZoneParams *ZoneParser::ParseXML(xml_node zonenode)
     ZoneParams *zone = nullptr;
     auto hasError = false;
 
+    static std::map<std::string, AutonGrid::XGRID> X_xmlStringToGridEnumMap{
+        {"X_A", AutonGrid::XGRID::X_A},
+        {"X_B", AutonGrid::XGRID::X_B},
+        {"X_C", AutonGrid::XGRID::X_C},
+        {"X_D", AutonGrid::XGRID::X_D},
+        {"X_E", AutonGrid::XGRID::X_E},
+        {"X_F", AutonGrid::XGRID::X_F},
+        {"X_G", AutonGrid::XGRID::X_G},
+        {"X_H", AutonGrid::XGRID::X_H},
+        {"X_I", AutonGrid::XGRID::X_I},
+        {"X_J", AutonGrid::XGRID::X_J},
+        {"X_K", AutonGrid::XGRID::X_K},
+        {"X_L", AutonGrid::XGRID::X_L},
+        {"X_M", AutonGrid::XGRID::X_M},
+        {"X_N", AutonGrid::XGRID::X_N},
+        {"X_O", AutonGrid::XGRID::X_O},
+        {"X_P", AutonGrid::XGRID::X_P},
+        {"X_Q", AutonGrid::XGRID::X_Q},
+        {"X_R", AutonGrid::XGRID::X_R},
+        {"X_S", AutonGrid::XGRID::X_S},
+        {"X_T", AutonGrid::XGRID::X_T},
+        {"X_U", AutonGrid::XGRID::X_U},
+        {"X_V", AutonGrid::XGRID::X_V},
+        {"X_W", AutonGrid::XGRID::X_W},
+        {"X_X", AutonGrid::XGRID::X_X},
+        {"X_Y", AutonGrid::XGRID::X_Y},
+        {"X_Z", AutonGrid::XGRID::X_Z},
+        {"X_AA", AutonGrid::XGRID::X_AA}};
+    static std::map<std::string, AutonGrid::YGRID> Y_xmlStringToGridEnumMap{
+        {"Y_1", AutonGrid::YGRID::Y_1},
+        {"Y_2", AutonGrid::YGRID::Y_2},
+        {"Y_3", AutonGrid::YGRID::Y_3},
+        {"Y_4", AutonGrid::YGRID::Y_4},
+        {"Y_5", AutonGrid::YGRID::Y_5},
+        {"Y_6", AutonGrid::YGRID::Y_6},
+        {"Y_7", AutonGrid::YGRID::Y_7},
+        {"Y_8", AutonGrid::YGRID::Y_8},
+        {"Y_9", AutonGrid::YGRID::Y_9},
+        {"Y_10", AutonGrid::YGRID::Y_10},
+        {"Y_11", AutonGrid::YGRID::Y_11},
+        {"Y_12", AutonGrid::YGRID::Y_12},
+        {"Y_13", AutonGrid::YGRID::Y_13},
+        {"Y_14", AutonGrid::YGRID::Y_14},
+        {"Y_15", AutonGrid::YGRID::Y_15},
+        {"Y_16", AutonGrid::YGRID::Y_16},
+        {"Y_17", AutonGrid::YGRID::Y_17},
+        {"Y_18", AutonGrid::YGRID::Y_18},
+        {"Y_19", AutonGrid::YGRID::Y_19},
+        {"Y_20", AutonGrid::YGRID::Y_20},
+        {"Y_21", AutonGrid::YGRID::Y_21},
+        {"Y_22", AutonGrid::YGRID::Y_22},
+        {"Y_23", AutonGrid::YGRID::Y_23},
+        {"Y_24", AutonGrid::YGRID::Y_24},
+        {"Y_25", AutonGrid::YGRID::Y_25},
+        {"Y_26", AutonGrid::YGRID::Y_26},
+        {"Y_27", AutonGrid::YGRID::Y_27},
+        {"Y_28", AutonGrid::YGRID::Y_28},
+        {"Y_29", AutonGrid::YGRID::Y_29},
+        {"Y_30", AutonGrid::YGRID::Y_30},
+        {"Y_31", AutonGrid::YGRID::Y_31},
+        {"Y_32", AutonGrid::YGRID::Y_32},
+        {"Y_33", AutonGrid::YGRID::Y_33},
+        {"Y_34", AutonGrid::YGRID::Y_34},
+        {"Y_35", AutonGrid::YGRID::Y_35},
+        {"Y_36", AutonGrid::YGRID::Y_36},
+        {"Y_37", AutonGrid::YGRID::Y_37},
+        {"Y_38", AutonGrid::YGRID::Y_38},
+        {"Y_39", AutonGrid::YGRID::Y_39},
+        {"Y_40", AutonGrid::YGRID::Y_40},
+        {"Y_41", AutonGrid::YGRID::Y_41},
+        {"Y_42", AutonGrid::YGRID::Y_42},
+        {"Y_43", AutonGrid::YGRID::Y_43},
+        {"Y_44", AutonGrid::YGRID::Y_44},
+        {"Y_45", AutonGrid::YGRID::Y_45},
+        {"Y_46", AutonGrid::YGRID::Y_46},
+        {"Y_47", AutonGrid::YGRID::Y_47},
+        {"Y_48", AutonGrid::YGRID::Y_48},
+        {"Y_49", AutonGrid::YGRID::Y_49},
+        {"Y_50", AutonGrid::YGRID::Y_50},
+        {"Y_51", AutonGrid::YGRID::Y_51},
+        {"Y_52", AutonGrid::YGRID::Y_52},
+        {"Y_53", AutonGrid::YGRID::Y_53},
+        {"Y_54", AutonGrid::YGRID::Y_54}};
+
     AutonGrid::XGRID xgrid1 = AutonGrid::XGRID::NO_VALUE;
     AutonGrid::YGRID ygrid1 = AutonGrid::YGRID::NONE;
     AutonGrid::XGRID xgrid2 = AutonGrid::XGRID::NO_VALUE;
@@ -44,34 +128,50 @@ ZoneParams *ZoneParser::ParseXML(xml_node zonenode)
 
         if (strcmp(attr.name(), "xgrid1") == 0)
         {
-            auto val = attr.as_int();
-            if (val > AutonGrid::XGRID::NO_VALUE && val < AutonGrid::XGRID::EXCEEDING_VALUE) // determine if grid coordinates are in range of the enum
+            auto itr = X_xmlStringToGridEnumMap.find(attr.value());
+            if (itr != X_xmlStringToGridEnumMap.end())
             {
-                xgrid1 = static_cast<AutonGrid::XGRID>(val); // casting that val to one of the enum values
+                xgrid1 = itr->second;
+            }
+            else
+            {
+                hasError = true;
             }
         }
         if (strcmp(attr.name(), "ygrid1") == 0)
         {
-            auto val = attr.as_int();
-            if (val > AutonGrid::YGRID::NONE && val < AutonGrid::YGRID::EXCEEDED)
+            auto itr = Y_xmlStringToGridEnumMap.find(attr.value());
+            if (itr != Y_xmlStringToGridEnumMap.end())
             {
-                ygrid1 = static_cast<AutonGrid::YGRID>(val);
+                ygrid1 = itr->second;
+            }
+            else
+            {
+                hasError = true;
             }
         }
         if (strcmp(attr.name(), "xgrid2") == 0)
         {
-            auto val = attr.as_int();
-            if (val > AutonGrid::XGRID::NO_VALUE && val < AutonGrid::XGRID::EXCEEDING_VALUE)
+            auto itr = X_xmlStringToGridEnumMap.find(attr.value());
+            if (itr != X_xmlStringToGridEnumMap.end())
             {
-                xgrid2 = static_cast<AutonGrid::XGRID>(val);
+                xgrid2 = itr->second;
+            }
+            else
+            {
+                hasError = true;
             }
         }
         if (strcmp(attr.name(), "ygrid2") == 0)
         {
-            auto val = attr.as_int();
-            if (val > AutonGrid::YGRID::NONE && val < AutonGrid::YGRID::EXCEEDED)
+            auto itr = Y_xmlStringToGridEnumMap.find(attr.value());
+            if (itr != Y_xmlStringToGridEnumMap.end())
             {
-                ygrid2 = static_cast<AutonGrid::YGRID>(val);
+                ygrid2 = itr->second;
+            }
+            else
+            {
+                hasError = true;
             }
         }
     }
