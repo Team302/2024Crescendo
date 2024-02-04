a7917c4d5c4134da2505f9cbacf4ed1e3a1d5658
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2024-02-04 13:09:26 -0500

    fixed spelling

--- src/main/cpp/utils/FieldConsts.cpp
+++ src/main/cpp/utils/FieldConsts.cpp
@@ -12,7 +12,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
 //====================================================================================================================================================
-#include "FeildConsts.h"
+#include "FieldConsts.h"
 using namespace std;
 
 frc::Pose3d FieldConsts::GetFeildElement(FIELD_ELEMENT element)
