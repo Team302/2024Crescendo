9b82d50ff5248f9cab68ec106bbad93ebc1418ee
Tanay Patel <adambots.tanay@gmail.com>
2024-02-05 19:01:42 -0500

    No longer burning the factory default into the spark max every time we initialize the robot

--- src/main/cpp/hw/DragonSparkMax.cpp
+++ src/main/cpp/hw/DragonSparkMax.cpp
@@ -42,7 +42,7 @@ DragonSparkMax::DragonSparkMax(int id,
                                                                           m_reverseLimitSwitch(m_spark->GetReverseLimitSwitch(m_reverseType)),
                                                                           m_calcStruc(calcStruc)
 {
-    m_spark->RestoreFactoryDefaults(true);
+    m_spark->RestoreFactoryDefaults();
     m_pidController.SetOutputRange(-1.0, 1.0, 0);
     m_pidController.SetOutputRange(-1.0, 1.0, 1);
     m_spark->SetOpenLoopRampRate(0.09); // 0.2 0.25
