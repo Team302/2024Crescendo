958a174259dc6800e14e342f34248fbc5cc2be7b
Frank0624789 <89255020+Frank0624789@users.noreply.github.com>
2024-02-06 16:21:20 -0800

    Completed updating FaceGamePiece's UpdateChassisSpeeds method and cleaned the h and cpp up

--- src/main/cpp/chassis/headingStates/FaceGamePiece.h
+++ src/main/cpp/chassis/headingStates/FaceGamePiece.h
@@ -26,18 +26,4 @@ public:
     ~FaceGamePiece();
 
     void UpdateChassisSpeeds(ChassisMovement &chassisMovement) override;
-
-private:
-    units::angular_velocity::radians_per_second_t limitAngularVelocityToBetweenMinAndMax(units::angular_velocity::radians_per_second_t angularSpeed);
-    /*
-    bool AtTargetAngle(std::shared_ptr<DragonVisionTarget> targetData, units::angle::radian_t *error);
-
-    DragonLimelight::PIPELINE_MODE m_pipelineMode;
-    DragonVision *m_vision;
-
-    // Angular movement settings
-    const double m_minimumOmega_radps = 0.7;
-    const double m_maximumOmega_radps = 1.5;
-    const double m_AngularTolerance_rad = std::numbers::pi * 2.0 / 180.0;
-    double m_visionKP_Angle = 6.0;*/
 };
