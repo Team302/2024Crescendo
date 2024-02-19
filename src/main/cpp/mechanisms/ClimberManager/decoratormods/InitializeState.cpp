335ad8bed36569173f10a5d25206fd67e5d83ea1
Tanay Patel <adambots.tanay@gmail.com>
2024-02-19 16:54:05 -0500

    Climber tuned for now

--- src/main/cpp/mechanisms/ClimberManager/decoratormods/InitializeState.cpp
+++ src/main/cpp/mechanisms/ClimberManager/decoratormods/InitializeState.cpp
@@ -45,8 +45,8 @@ void InitializeState::Init()
 	Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("ArrivedAt"), string("InitializeState"), string("init"));
 
 	m_genState->Init();
-	m_mechanism->getleftClimber()->SetSelectedSensorPosition(34);
-	m_mechanism->getrightClimber()->SetSelectedSensorPosition(33.5);
+	m_mechanism->getleftClimber()->SetSelectedSensorPosition(30.5);
+	m_mechanism->getrightClimber()->SetSelectedSensorPosition(30.5);
 }
 
 void InitializeState::Run()
@@ -70,5 +70,5 @@ bool InitializeState::IsTransitionCondition(bool considerGamepadTransitions)
 {
 	// To get the current state use m_mechanism->GetCurrentState()
 
-	return (m_mechanism->IsEnabled() && false);
+	return (m_mechanism->IsEnabled());
 }
