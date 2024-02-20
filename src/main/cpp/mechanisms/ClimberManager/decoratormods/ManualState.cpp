4dff10145e1ae7b377ab766ec2f482c55d11e433
Tanay Patel <adambots.tanay@gmail.com>
2024-02-20 11:05:48 -0500

    Idle Speed working well

--- src/main/cpp/mechanisms/ClimberManager/decoratormods/ManualState.cpp
+++ src/main/cpp/mechanisms/ClimberManager/decoratormods/ManualState.cpp
@@ -54,6 +54,8 @@ void ManualState::Run()
 	{
 		double delta = 6.0 * 0.03 * (TeleopControl::GetInstance()->GetAxisValue(TeleopControlFunctions::MANUAL_CLIMB)); // changing by 6 in/s * 0.05 for 20 ms loop time * controller input
 		m_target += delta;
+		if (m_target < 6.0)
+			m_target = 6.0;
 		m_mechanism->UpdateTarget(RobotElementNames::MOTOR_CONTROLLER_USAGE::CLIMBER_MANAGER_RIGHT_CLIMBER, m_target);
 		m_mechanism->UpdateTarget(RobotElementNames::MOTOR_CONTROLLER_USAGE::CLIMBER_MANAGER_LEFT_CLIMBER, m_target);
 	}
