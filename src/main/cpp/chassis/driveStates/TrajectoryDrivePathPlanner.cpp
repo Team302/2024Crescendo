388b59efe9e724c93b29cdb0cd9431db76b101ed
Aden25 <72420903+Aden25@users.noreply.github.com>
2024-02-24 14:07:32 -0500

    Rotation change

--- src/main/cpp/chassis/driveStates/TrajectoryDrivePathPlanner.cpp
+++ src/main/cpp/chassis/driveStates/TrajectoryDrivePathPlanner.cpp
@@ -33,7 +33,7 @@ TrajectoryDrivePathPlanner::TrajectoryDrivePathPlanner(RobotDrive *robotDrive) :
                                                                                  m_robotDrive(robotDrive),
                                                                                  // TODO need to tune this also update radius as it is probably wrong
                                                                                  m_holonomicController(pathplanner::PIDConstants(4.0, 1.25, 0.0),
-                                                                                                       pathplanner::PIDConstants(3.0, 1.0, 0.0),
+                                                                                                       pathplanner::PIDConstants(2.5, 1.0, 0.0),
                                                                                                        robotDrive->GetChassis()->GetMaxSpeed(),
                                                                                                        units::length::inch_t(sqrt((robotDrive->GetChassis()->GetWheelBase().to<double>() * robotDrive->GetChassis()->GetWheelBase().to<double>() + robotDrive->GetChassis()->GetTrack().to<double>() * robotDrive->GetChassis()->GetTrack().to<double>()))),
                                                                                                        units::time::second_t(0.02)),
