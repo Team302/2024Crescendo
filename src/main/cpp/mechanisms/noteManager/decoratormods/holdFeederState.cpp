b85d47a4281f3270c18e73bdc16634a225055b84
Tanay Patel <adambots.tanay@gmail.com>
2024-02-24 00:14:06 -0500

    Cleaned up Logging and added ReadyManualState Transition

--- src/main/cpp/mechanisms/noteManager/decoratormods/holdFeederState.cpp
+++ src/main/cpp/mechanisms/noteManager/decoratormods/holdFeederState.cpp
@@ -51,9 +51,7 @@ void holdFeederState::Init()
 void holdFeederState::Run()
 {
 	// Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("ArrivedAt"), string("holdFeederState"), string("run"));
-	Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("Launcher"), string("holdFeederState "), m_mechanism->HasVisionTarget() ? "Has vision target" : "Does not have vision target");
 	Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, string("Launcher"), string("Angle"), m_mechanism->getlauncherAngle()->GetCounts());
-
 	m_genState->Run();
 }
 
@@ -74,7 +72,7 @@ bool holdFeederState::IsTransitionCondition(bool considerGamepadTransitions)
 	bool feederSensor = m_mechanism->getfeederSensor()->Get();
 	bool launcherSensor = m_mechanism->getlauncherSensor()->Get();
 	auto currentstate = m_mechanism->GetCurrentState();
-	bool visionTargetAcquired = m_mechanism->HasVisionTarget(); // todo be set later with std::optional<VisionData> optionalvisionData = m_vision->GetVisionData(DragonVision::VISION_ELEMENT::SPEAKER);
+	bool visionTargetAcquired = m_mechanism->HasVisionTarget();
 
-	return ((feederSensor && launcherSensor && (currentstate == m_mechanism->STATE_FEEDER_INTAKE))); //|| ((currentstate == m_mechanism->STATE_READY_AUTO_LAUNCH) && visionTargetAcquired == false));
+	return ((feederSensor && launcherSensor && (currentstate == m_mechanism->STATE_FEEDER_INTAKE)) || ((currentstate == m_mechanism->STATE_READY_AUTO_LAUNCH) && visionTargetAcquired == false));
 }
