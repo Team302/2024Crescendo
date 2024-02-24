0262ea12eae43fdc7db4be371ad3b3a728f0a717
David Muscat <50778601+muscatd75@users.noreply.github.com>
2024-02-24 15:54:35 -0500

    fixed chaser pattern and switching patterns

--- src/main/cpp/driveteamfeedback/DriverFeedback.cpp
+++ src/main/cpp/driveteamfeedback/DriverFeedback.cpp
@@ -49,17 +49,34 @@ void DriverFeedback::UpdateLEDStates()
     // reset controller rumble
     // TeleopControl::GetInstance()->SetRumble(0, false, false);
 
+    oldState = currentState;
+
     if (m_climbMode == RobotStateChanges::ClimbMode::ClimbModeOn)
     {
-        m_LEDStates->SolidColorPattern(DragonLeds::RED);
+        currentState = DragonLeds::RED;
+        if (oldState != currentState)
+        {
+            m_LEDStates->ResetVariables();
+        }
+        m_LEDStates->ChaserPattern(DragonLeds::RED);
     }
     else if (m_scoringMode == RobotStateChanges::ScoringMode::Launcher)
     {
+        currentState = DragonLeds::GREEN;
+        if (oldState != currentState)
+        {
+            m_LEDStates->ResetVariables();
+        }
         m_LEDStates->SolidColorPattern(DragonLeds::GREEN);
     }
     else if (m_scoringMode == RobotStateChanges::ScoringMode::Placer)
     {
-        m_LEDStates->SolidColorPattern(DragonLeds::WHITE);
+        currentState = DragonLeds::BLUE;
+        if (oldState != currentState)
+        {
+            m_LEDStates->ResetVariables();
+        }
+        m_LEDStates->ClosingInChaserPattern(DragonLeds::BLUE);
     }
 }
 
