7ebd0d561a58af35b8c9d062b92ccf1e5594a288
reslip <reslip@icloud.com>
2024-02-26 09:09:29 -0500

    fixes for sim
    Fixes to data trace socket to allow builds for simulation and corrections to field constants unit test

--- src/test/cpp/utils/FieldConstantsTest.cpp
+++ src/test/cpp/utils/FieldConstantsTest.cpp
@@ -4,17 +4,16 @@
 class FieldConstantsTest : public ::testing::Test
 {
     protected:
-    FieldConstants fieldConstants = FieldConstants.getInstance();
+    
 };
 
 TEST_F(FieldConstantsTest, GetFieldElementTest)
 {
-    FIELD_ELEMENT element = FIELD_ELEMENT::BLUE_CENTER_STAGE;
+    auto fieldConstants = FieldConstants::GetInstance();
 
-    frc::Pose3d result = fieldConstants.GetFieldElement(element);
-    EXPECT_DOUBLE_EQ(result.Get(X).to<double>(), 5.32);
-    EXPECT_DOUBLE_EQ(result.Get(Y).to<double>(), 4.11);
-    EXPECT_DOUBLE_EQ(result.Get(Yaw).to<double>(), 0.0);
-    EXPECT_DOUBLE_EQ(result.Get(Rotation).GetDegrees().to<double>(), 0.0);
-    EXPECT_DOUBLE_NE(result.Get(X).to<double>(), 4);
+    frc::Pose3d result = fieldConstants->GetFieldElement(fieldConstants->FIELD_ELEMENT::BLUE_CENTER_STAGE);
+    EXPECT_DOUBLE_EQ(result.X().to<double>(), 5.32);
+    EXPECT_DOUBLE_EQ(result.Y().to<double>(), 4.11);
+    EXPECT_DOUBLE_EQ(result.Z().to<double>(), 1.3200000000000001);
+    EXPECT_NE(result.Z().to<double>(), 4);
 }
