87eccdcb50e41be29a49bbf3b4a6a132ee051fd4
Tan560 <97492534+Tan560@users.noreply.github.com>
2024-02-28 16:29:21 -0500

    Slowed Manual Launch down and fixed config for auto/manual launch

--- src/main/cpp/mechanisms/noteManager/decoratormods/autoLaunchState.cpp
+++ src/main/cpp/mechanisms/noteManager/decoratormods/autoLaunchState.cpp
@@ -46,6 +46,13 @@ void autoLaunchState::Init()
 	m_genState->Init();
 	m_targetAngle = m_mechanism->getlauncherAngle()->GetCounts();
 	m_mechanism->UpdateTarget(RobotElementNames::MOTOR_CONTROLLER_USAGE::NOTE_MANAGER_LAUNCHER_ANGLE, m_targetAngle);
+
+	double distanceFromTarget = GetVisionDistance().to<double>();
+	if (distanceFromTarget < 1.5)
+	{
+		m_mechanism->UpdateTarget(RobotElementNames::MOTOR_CONTROLLER_USAGE::NOTE_MANAGER_LAUNCHER_TOP, units::angular_velocity::revolutions_per_minute_t(units::angular_velocity::radians_per_second_t(350)));
+		m_mechanism->UpdateTarget(RobotElementNames::MOTOR_CONTROLLER_USAGE::NOTE_MANAGER_LAUNCHER_BOTTOM, units::angular_velocity::revolutions_per_minute_t(units::angular_velocity::radians_per_second_t(350)));
+	}
 }
 
 void autoLaunchState::Run()
