e78523839096f3853722f85df6ad43f811c8b7f4
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2024-03-09 16:45:48 -0500

    swerve issues

--- src/main/cpp/chassis/ChassisConfigChassis_9997.h
+++ src/main/cpp/chassis/ChassisConfigChassis_9997.h
@@ -33,28 +33,28 @@ private:
 	std::string m_canbusName = std::string("rio");
 	const int m_leftfrontdriveID = 15;
 	const int m_leftfrontturnID = 14;
-	const double m_leftfrontOffset = 0.187255859375;
+	const double m_leftfrontOffset = 0.1904296875;
 	const bool m_leftfrontdriveInvert = true;
 	const bool m_leftfrontturnInvert = true;
 	const bool m_leftfrontcancoderInvert = true;
 
 	const int m_leftbackdriveID = 1;
 	const int m_leftbackturnID = 0;
-	const double m_leftbackOffset = -0.181884765625;
+	const double m_leftbackOffset = -0.182373046875;
 	const bool m_leftbackdriveInvert = true;
 	const bool m_leftbackturnInvert = true;
 	const bool m_leftbackcancoderInvert = true;
 
 	const int m_rightfrontdriveID = 13;
 	const int m_rightfrontturnID = 12;
-	const double m_rightfrontOffset = 0.212158203125;
+	const double m_rightfrontOffset = 0.194091796875;
 	const bool m_rightfrontdriveInvert = true;
 	const bool m_rightfrontturnInvert = true;
 	const bool m_rightfrontcancoderInvert = true;
 
 	const int m_rightbackdriveID = 3;
 	const int m_rightbackturnID = 2;
-	const double m_rightbackOffset = -0.1357421875;
+	const double m_rightbackOffset = -0.14306640625;
 	const bool m_rightbackdriveInvert = true;
 	const bool m_rightbackturnInvert = true;
 	const bool m_rightbackcancoderInvert = true;
