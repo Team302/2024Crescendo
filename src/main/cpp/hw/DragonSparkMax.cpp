6b64ee08e2e1f51f113bf550a0a1c95e5b438f08
Joe Witcpalek <joe.witcpalek@gmail.com>
2024-03-09 13:54:11 -0500

    SetCANTimeout to 0

--- src/main/cpp/hw/DragonSparkMax.cpp
+++ src/main/cpp/hw/DragonSparkMax.cpp
@@ -43,6 +43,7 @@ DragonSparkMax::DragonSparkMax(int id,
                                                                           m_calcStruc(calcStruc)
 {
     m_spark->RestoreFactoryDefaults();
+    m_spark->SetCANTimeout(0);
     m_pidController.SetOutputRange(-1.0, 1.0, 0);
     m_pidController.SetOutputRange(-1.0, 1.0, 1);
     m_spark->SetOpenLoopRampRate(0.09); // 0.2 0.25
