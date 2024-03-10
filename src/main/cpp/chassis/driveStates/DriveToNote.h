fa7f6e64e5cd2985c473420f54c647e3ef88b06a
Tan560 <adambots.tanay@gmail.com>
2024-03-10 14:30:31 -0400

    Update DriveToNote.h

--- src/main/cpp/chassis/driveStates/DriveToNote.h
+++ src/main/cpp/chassis/driveStates/DriveToNote.h
@@ -38,7 +38,6 @@ public:
 
     pathplanner::PathPlannerTrajectory CreateDriveToNote();
     void Init(ChassisMovement &chassisMovement) override;
-    std::array<frc::SwerveModuleState, 4> UpdateSwerveModuleStates(ChassisMovement &chassisMovement) override;
     pathplanner::PathPlannerTrajectory GetTrajectory() const { return m_trajectory; }
 
 private:
