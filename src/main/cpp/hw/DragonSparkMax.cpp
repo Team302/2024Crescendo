76326685fdd1797b10a7153a19bc9c1fc6358f99
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2024-04-12 21:43:48 -0400

    Update DragonSparkMax.cpp

--- src/main/cpp/hw/DragonSparkMax.cpp
+++ src/main/cpp/hw/DragonSparkMax.cpp
@@ -42,16 +42,16 @@ DragonSparkMax::DragonSparkMax(int id,
                                                                           m_reverseLimitSwitch(m_spark->GetReverseLimitSwitch(m_reverseType)),
                                                                           m_calcStruc(calcStruc)
 {
-    m_spark->RestoreFactoryDefaults();
-    // m_spark->SetCANTimeout(0);
-    m_pidController.SetOutputRange(-1.0, 1.0, 0);
-    m_pidController.SetOutputRange(-1.0, 1.0, 1);
-    m_spark->SetOpenLoopRampRate(0.09); // 0.2 0.25
-    m_spark->SetClosedLoopRampRate(0.02);
-    m_encoder.SetPosition(0);
-    SetRotationOffset(0);
-    m_forwardLimitSwitch.EnableLimitSwitch(false);
-    m_reverseLimitSwitch.EnableLimitSwitch(false);
+    // m_spark->RestoreFactoryDefaults();
+    // // m_spark->SetCANTimeout(0);
+    // m_pidController.SetOutputRange(-1.0, 1.0, 0);
+    // m_pidController.SetOutputRange(-1.0, 1.0, 1);
+    // m_spark->SetOpenLoopRampRate(0.09); // 0.2 0.25
+    // m_spark->SetClosedLoopRampRate(0.02);
+    // m_encoder.SetPosition(0);
+    // SetRotationOffset(0);
+    // m_forwardLimitSwitch.EnableLimitSwitch(false);
+    // m_reverseLimitSwitch.EnableLimitSwitch(false);
 }
 
 double DragonSparkMax::GetRotations()
@@ -76,83 +76,83 @@ int DragonSparkMax::GetID() const
 
 void DragonSparkMax::SetControlConstants(int slot, const ControlData &controlInfo)
 {
-    bool needToSet = false;
-    double target = 1.0;
-    switch (controlInfo.GetMode())
-    {
-    case ControlModes::PERCENT_OUTPUT:
-        m_controlType = CANSparkBase::ControlType ::kDutyCycle;
-        break;
-    case ControlModes::POSITION_INCH:
-        target = m_calcStruc.countsPerInch;
-        needToSet = std::abs(m_posConversion - target) > m_chgTolerance;
-        if (needToSet)
-        {
-            m_encoder.SetPositionConversionFactor(target);
-            m_posConversion = target;
-        }
-        m_controlType = CANSparkBase::ControlType::kPosition;
-        break;
-    case ControlModes::POSITION_DEGREES:
-        target = m_calcStruc.countsPerDegree;
-        needToSet = std::abs(m_posConversion - target) > m_chgTolerance;
-        if (needToSet)
-        {
-            m_encoder.SetPositionConversionFactor(target);
-            m_posConversion = target;
-        }
-        m_controlType = CANSparkBase::ControlType ::kPosition;
-        break;
-    case ControlModes::VELOCITY_RPS:
-        target = m_calcStruc.countsPerRev;
-        needToSet = std::abs(m_velConversion - target) > m_chgTolerance;
-        if (needToSet)
-        {
-            m_encoder.SetVelocityConversionFactor(target);
-            m_velConversion = target;
-        }
-        m_controlType = CANSparkBase::ControlType::kVelocity;
-        break;
+    // bool needToSet = false;
+    // double target = 1.0;
+    // switch (controlInfo.GetMode())
+    // {
+    // case ControlModes::PERCENT_OUTPUT:
+    //     m_controlType = CANSparkBase::ControlType ::kDutyCycle;
+    //     break;
+    // case ControlModes::POSITION_INCH:
+    //     target = m_calcStruc.countsPerInch;
+    //     needToSet = std::abs(m_posConversion - target) > m_chgTolerance;
+    //     if (needToSet)
+    //     {
+    //         m_encoder.SetPositionConversionFactor(target);
+    //         m_posConversion = target;
+    //     }
+    //     m_controlType = CANSparkBase::ControlType::kPosition;
+    //     break;
+    // case ControlModes::POSITION_DEGREES:
+    //     target = m_calcStruc.countsPerDegree;
+    //     needToSet = std::abs(m_posConversion - target) > m_chgTolerance;
+    //     if (needToSet)
+    //     {
+    //         m_encoder.SetPositionConversionFactor(target);
+    //         m_posConversion = target;
+    //     }
+    //     m_controlType = CANSparkBase::ControlType ::kPosition;
+    //     break;
+    // case ControlModes::VELOCITY_RPS:
+    //     target = m_calcStruc.countsPerRev;
+    //     needToSet = std::abs(m_velConversion - target) > m_chgTolerance;
+    //     if (needToSet)
+    //     {
+    //         m_encoder.SetVelocityConversionFactor(target);
+    //         m_velConversion = target;
+    //     }
+    //     m_controlType = CANSparkBase::ControlType::kVelocity;
+    //     break;
 
-    default:
-        // danger11!!!!
-        m_spark->Set(0);
-        break;
-    }
+    // default:
+    //     // danger11!!!!
+    //     m_spark->Set(0);
+    //     break;
+    // }
 
-    auto ctlSlot = (m_controlType == CANSparkBase::ControlType::kVelocity) ? m_velSlot : m_posSlot;
+    // auto ctlSlot = (m_controlType == CANSparkBase::ControlType::kVelocity) ? m_velSlot : m_posSlot;
 
-    target = controlInfo.GetP();
-    needToSet = std::abs(m_prevKp[ctlSlot] - target) > m_chgTolerance;
-    if (needToSet)
-    {
-        m_prevKp[ctlSlot] = target;
-        m_pidController.SetP(target, ctlSlot);
-    }
+    // target = controlInfo.GetP();
+    // needToSet = std::abs(m_prevKp[ctlSlot] - target) > m_chgTolerance;
+    // if (needToSet)
+    // {
+    //     m_prevKp[ctlSlot] = target;
+    //     m_pidController.SetP(target, ctlSlot);
+    // }
 
-    target = controlInfo.GetI();
-    needToSet = std::abs(m_prevKi[ctlSlot] - target) > m_chgTolerance;
-    if (needToSet)
-    {
-        m_prevKi[ctlSlot] = target;
-        m_pidController.SetI(target, ctlSlot);
-    }
+    // target = controlInfo.GetI();
+    // needToSet = std::abs(m_prevKi[ctlSlot] - target) > m_chgTolerance;
+    // if (needToSet)
+    // {
+    //     m_prevKi[ctlSlot] = target;
+    //     m_pidController.SetI(target, ctlSlot);
+    // }
 
-    target = controlInfo.GetD();
-    needToSet = std::abs(m_prevKd[ctlSlot] - target) > m_chgTolerance;
-    if (needToSet)
-    {
-        m_prevKd[ctlSlot] = target;
-        m_pidController.SetD(target, ctlSlot);
-    }
+    // target = controlInfo.GetD();
+    // needToSet = std::abs(m_prevKd[ctlSlot] - target) > m_chgTolerance;
+    // if (needToSet)
+    // {
+    //     m_prevKd[ctlSlot] = target;
+    //     m_pidController.SetD(target, ctlSlot);
+    // }
 
-    target = controlInfo.GetF();
-    needToSet = std::abs(m_prevKf[ctlSlot] - target) > m_chgTolerance;
-    if (needToSet)
-    {
-        m_prevKf[ctlSlot] = target;
-        m_pidController.SetFF(target, ctlSlot);
-    }
+    // target = controlInfo.GetF();
+    // needToSet = std::abs(m_prevKf[ctlSlot] - target) > m_chgTolerance;
+    // if (needToSet)
+    // {
+    //     m_prevKf[ctlSlot] = target;
+    //     m_pidController.SetFF(target, ctlSlot);
+    // }
 }
 
 void DragonSparkMax::EnableCurrentLimiting(bool enabled)
@@ -161,44 +161,44 @@ void DragonSparkMax::EnableCurrentLimiting(bool enabled)
 
 void DragonSparkMax::Set(double value)
 {
-    if (m_controlType == CANSparkBase::ControlType::kDutyCycle)
-    {
-        m_spark->Set(value);
-    }
-    else if (m_controlType == CANSparkBase::ControlType::kVelocity)
-    {
-        m_pidController.SetReference(value * 60, m_controlType, m_velSlot);
-    }
-    else
-    {
-        m_pidController.SetReference(value, m_controlType, m_posSlot);
-    }
+    // if (m_controlType == CANSparkBase::ControlType::kDutyCycle)
+    // {
+    //     m_spark->Set(value);
+    // }
+    // else if (m_controlType == CANSparkBase::ControlType::kVelocity)
+    // {
+    //     m_pidController.SetReference(value * 60, m_controlType, m_velSlot);
+    // }
+    // else
+    // {
+    //     m_pidController.SetReference(value, m_controlType, m_posSlot);
+    // }
 }
 
 void DragonSparkMax::SetRotationOffset(double rotations)
 {
-    m_outputRotationOffset = GetRotationsWithGearNoOffset() - rotations;
+    // m_outputRotationOffset = GetRotationsWithGearNoOffset() - rotations;
 }
 
 void DragonSparkMax::SetVoltageRamping(double ramping, double rampingClosedLoop)
 {
-    m_spark->SetOpenLoopRampRate(ramping);
-    m_spark->SetClosedLoopRampRate(ramping); // TODO: should closed and open be separate
+    // m_spark->SetOpenLoopRampRate(ramping);
+    // m_spark->SetClosedLoopRampRate(ramping); // TODO: should closed and open be separate
 
-    if (rampingClosedLoop >= 0)
-    {
-        m_spark->SetClosedLoopRampRate(rampingClosedLoop);
-    }
+    // if (rampingClosedLoop >= 0)
+    // {
+    //     m_spark->SetClosedLoopRampRate(rampingClosedLoop);
+    // }
 }
 
 void DragonSparkMax::EnableBrakeMode(bool enabled)
 {
-    m_spark->SetIdleMode(enabled ? rev::CANSparkMax::IdleMode::kBrake : rev::CANSparkMax::IdleMode::kCoast);
+    // m_spark->SetIdleMode(enabled ? rev::CANSparkMax::IdleMode::kBrake : rev::CANSparkMax::IdleMode::kCoast);
 }
 
 void DragonSparkMax::Invert(bool inverted)
 {
-    m_spark->SetInverted(inverted);
+    // m_spark->SetInverted(inverted);
 }
 
 double DragonSparkMax::GetRotationsWithGearNoOffset() const
@@ -208,7 +208,7 @@ double DragonSparkMax::GetRotationsWithGearNoOffset() const
 
 void DragonSparkMax::InvertEncoder(bool inverted)
 {
-    m_encoder.SetInverted(inverted);
+    // m_encoder.SetInverted(inverted);
 }
 
 CANSparkMax *DragonSparkMax::GetSparkMax()
@@ -218,12 +218,12 @@ CANSparkMax *DragonSparkMax::GetSparkMax()
 
 void DragonSparkMax::SetSmartCurrentLimiting(int limit)
 {
-    m_spark->SetSmartCurrentLimit(limit);
+    // m_spark->SetSmartCurrentLimit(limit);
 }
 
 void DragonSparkMax::SetSecondaryCurrentLimiting(int limit, int duration)
 {
-    m_spark->SetSecondaryCurrentLimit(limit, duration);
+    // m_spark->SetSecondaryCurrentLimit(limit, duration);
 }
 
 double DragonSparkMax::GetCurrent()
