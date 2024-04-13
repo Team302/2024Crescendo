e9a2445d0138806e2886c548a6beac6e3aa9987d
alyssaperson1 <alyssa.acker@stu.lok12.org>
2024-04-12 21:02:33 -0400

    Put in if you across the line
    Review tonight: convert units to meters and figure out what units the poses are in

--- src/main/cpp/auton/drivePrimitives/DrivePathPlanner.cpp
+++ src/main/cpp/auton/drivePrimitives/DrivePathPlanner.cpp
@@ -154,6 +154,29 @@ bool DrivePathPlanner::IsDone()
         return true;
     }
 
+    else if (FMSData::GetInstance()->GetAllianceColor() == frc::DriverStation::kBlue)
+    {
+        return m_chassis->GetPose().X() >= (m_centerLine + m_offset);
+
+        frc::Pose2d currentPose2d = m_chassis->GetPose();
+        frc::Pose2d targetPose = Pose2d(m_chassis->GetPose().X() - 0.5, m_chassis->GetPose().Y(), m_chassis->GetPose().Rotation());
+
+        auto path = PathPlannerPath::fromPathFile(m_pathname);
+        auto pathConstraints = path.get()->getGlobalConstraints();
+
+        std::vector<frc::Pose2d>
+            poses{currentPose2d, targetPose};
+
+        std::vector<frc::Translation2d> notebezierPoints = PathPlannerPath::bezierFromPoses(poses);
+        auto notepath = std::make_shared<PathPlannerPath>(notebezierPoints,
+                                                          PathConstraints(pathConstraints.getMaxVelocity(), pathConstraints.getMaxAcceleration(), pathConstraints.getMaxAngularVelocity(), pathConstraints.getMaxAngularAcceleration()),
+                                                          GoalEndState(1.0_mps, m_chassis->GetPose().Rotation().Degrees(), true));
+    }
+    else
+    {
+        return m_chassis->GetPose().X() <= (m_centerLine - m_offset);
+    }
+
     if (m_checkDriveToNote && !m_switchedToVisionDrive)
     {
         CheckForDriveToNote();
