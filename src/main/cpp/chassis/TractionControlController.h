4f82ae6a8bf7a0872292c628e014d255520694d8
Tan560 <97492534+Tan560@users.noreply.github.com>
2024-09-17 16:15:38 -0400

    fixed build issues

--- src/main/cpp/chassis/TractionControlController.h
+++ src/main/cpp/chassis/TractionControlController.h
@@ -1,9 +1,34 @@
+//====================================================================================================================================================
+// Copyright 2024 Lake Orion Robotics FIRST Team 302
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//====================================================================================================================================================
+
+#pragma once
+
+// C++ Includes
 #include <vector>
 #include <cmath>
+
+// FRC Includes
 #include "units/angle.h"
 #include "units/angular_velocity.h"
 #include "units/length.h"
 #include "units/velocity.h"
+#include "frc/filter/Debouncer.h"
+
+// Team 302 Includes
+
+using namespace frc;
 
 class TractionControlController
 {
@@ -29,8 +54,9 @@ private:
     static constexpr double MAX_SLIP_RATIO = 0.40;
     static constexpr int SIGMOID_K = 10;
     static constexpr double INERTIAL_VELOCITY_THRESHOLD = 0.01;
-    static constexpr double MIN_SLIPPING_TIME = 1.1;
     static constexpr double m_robotLoopRate_Hz = 50.0;
+
+    units::second_t MIN_SLIPPING_TIME = units::second_t(1.1);
 
     double m_optimalSlipRatio;
     double m_mass;
