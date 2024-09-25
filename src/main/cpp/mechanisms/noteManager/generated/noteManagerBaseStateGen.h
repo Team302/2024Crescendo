27f80fa7f3c12ef14d56c0bf5645b8d410ff64c3
Frank0624789 <89255020+Frank0624789@users.noreply.github.com>
2024-09-25 18:54:28 -0400

    Update noteManagerBaseStateGen.h

--- src/main/cpp/mechanisms/noteManager/generated/noteManagerBaseStateGen.h
+++ src/main/cpp/mechanisms/noteManager/generated/noteManagerBaseStateGen.h
@@ -97,7 +97,7 @@ public:
 	virtual bool AtTargetSolenoidStates() const;
 	virtual bool AtTargetServoStates() const;
 
-	bool isEnabled(RobotElementNames::MOTOR_CONTROLLER_USAGE identifier);
+	bool IsEnabled(RobotElementNames::MOTOR_CONTROLLER_USAGE identifier);
 
 	noteManagerGen *GetnoteManager() { return m_noteManager; }
 
